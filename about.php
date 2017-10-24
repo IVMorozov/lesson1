1d66d9ebf782a74b824fe3e09f5fc0ef9dd4fdf7
IVMorozov <33038726+IVMorozov@users.noreply.github.com>
2017-10-24 22:06:03 +0300

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,17 @@ $userName = 'Илья';
 $userAge = '35';
 $userEmail = 'morozovXXXX@ya.ru';
 $userCity = 'Волгоград';
-
-echo 'Имя'. ' ' . $userName
+$userinfo = 'Банк России, экономист';
 
 ?>
+<table border="0">
+    <caption><h1>Страница пользователя Илья</h1></caption>
+    <tr><td class="firstcol">Имя</td><td><?= $userName ?></td></tr>
+    <tr><td class="firstcol">Возраст</td><td><?= $userAge ?></td></tr>
+    <tr><td class="firstcol">Адрес электронной почты</td><td><?= $userEmail ?></td></tr>
+    <tr><td class="firstcol">Город</td><td><?= $userCity ?></td></tr>
+    <tr><td class="firstcol">О себе</td><td><?= $userinfo ?></td></tr>
+</table>
+<style>
+    .firstcol {width: 300px;}
+</style>
